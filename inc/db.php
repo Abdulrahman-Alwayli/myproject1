a4fef0f56066bd7a4448cca90a4c7d3571be9c0b
Abdulrahman Alwayli <146121747+Abdulrahman-Alwayli@users.noreply.github.com>
2023-09-26 21:41:16 +0300

    Add files via upload

---
+++ inc/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect('localhost' , 'root' , 'root' , 'd7project');
+
+if(!$con)
+{
+    echo mysqli_connect_errno();
+}
+
---
+++ parts/footer.php
@@ -0,0 +1,6 @@
+</div>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/loader.js"></script>
+    <script src="./js/Script.js"></script>
+</body>
+</html>
